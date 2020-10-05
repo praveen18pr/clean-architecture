cc740d431aa9bf0c1bbb67155f451d6ee0e89254
Gayathri <gayathri.capp@vensyx.com>
2020-10-05 11:45:55 +0530

    Added data binding support, recyclerview and adapter for news article

--- app/src/main/kotlin/com/praveen/cleanarchitecture/viewmodels/MainActivityViewModel.kt
+++ app/src/main/kotlin/com/praveen/cleanarchitecture/viewmodels/MainActivityViewModel.kt
@@ -1,7 +1,10 @@
 package com.praveen.cleanarchitecture.viewmodels
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.praveen.domain.NewsArticle
 import com.praveen.domain.usecases.UseCaseFactory
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
@@ -13,12 +16,17 @@ class MainActivityViewModel : ViewModel(), KoinComponent {
     // TODO learn and write test cases
     private val useCaseFactory by inject<UseCaseFactory>()
 
+    private val _newsListLiveData = MutableLiveData<List<NewsArticle>>()
+    val newsListLiveData: LiveData<List<NewsArticle>>
+        get() = _newsListLiveData
+
     fun fetchNewsList() {
         viewModelScope.launch {
             val fetchNewsList = useCaseFactory.fetchNewsList()
             fetchNewsList.collect {
                 println("Current thread :${Thread.currentThread().name}")
                 println("The news List size is : ${it.status}")
+                _newsListLiveData.value = it.results
             }
         }
     }
